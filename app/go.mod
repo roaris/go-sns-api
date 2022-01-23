ccdd7c9a207249ea388f2a4d16b616e8ff6e4dcd
roaris <one0803kyopro@gmail.com>
2022-01-24 06:33:24 +0900

    mysqlに接続する

--- app/go.mod
+++ app/go.mod
@@ -1,3 +1,8 @@
 module github.com/roaris/go_sns_api
 
 go 1.16
+
+require (
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
+)
--- app/main.go
+++ app/main.go
@@ -1,8 +1,34 @@
 package main
 
-import "net/http"
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+
+	_ "github.com/go-sql-driver/mysql"
+)
 
 func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	mysql_user := os.Getenv("MYSQL_USER")
+	mysql_password := os.Getenv("MYSQL_PASSWORD")
+	mysql_host := os.Getenv("MYSQL_HOST")
+	mysql_port := os.Getenv("MYSQL_PORT")
+	mysql_database := os.Getenv("MYSQL_DATABASE")
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", mysql_user, mysql_password, mysql_host, mysql_port, mysql_database)
+	_, err = sql.Open("mysql", dsn)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
 	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("pong"))
