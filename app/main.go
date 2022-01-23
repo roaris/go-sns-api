e2a3968e32ff5eccca1a3e79f6487992d5a381e0
roaris <one0803kyopro@gmail.com>
2022-01-24 02:46:20 +0900

    pingpong

--- app/main.go
+++ app/main.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import "net/http"
 
 func main() {
-	fmt.Println("HelloWorld")
+	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("pong"))
+	})
+
+	http.ListenAndServe(":8080", nil)
 }
