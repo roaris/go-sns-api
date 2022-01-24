341b192b270346e7e4db37ca25835b3f370cbd11
roaris <one0803kyopro@gmail.com>
2022-01-24 16:43:18 +0900

    postsテーブルを作成

--- app/main.go
+++ app/main.go
@@ -5,11 +5,19 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 	"github.com/joho/godotenv"
 )
+
+type Post struct {
+	ID        int
+	Content   string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
 
 func main() {
 	err := godotenv.Load(".env")
@@ -23,10 +31,12 @@ func main() {
 	mysqlPort := os.Getenv("MYSQL_PORT")
 	mysqlDatabase := os.Getenv("MYSQL_DATABASE")
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", mysqlUser, mysqlPassword, mysqlHost, mysqlPort, mysqlDatabase)
-	_, err = gorm.Open("mysql", dsn)
+	db, err := gorm.Open("mysql", dsn)
 	if err != nil {
 		log.Fatalln(err)
 	}
+
+	db.AutoMigrate(&Post{}) // postsテーブルの作成
 
 	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
