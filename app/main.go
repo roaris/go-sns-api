08e98278a9329ae05c3b88064531fa5262047a6d
roaris <one0803kyopro@gmail.com>
2022-01-24 18:31:48 +0900

    GET /api/v1/posts/:id を作成

--- app/main.go
+++ app/main.go
@@ -15,10 +15,10 @@ import (
 )
 
 type Post struct {
-	ID        int
-	Content   string
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	ID        int       `json:"id"`
+	Content   string    `json:"content"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
 
 type PostRequest struct {
@@ -36,7 +36,7 @@ func main() {
 	mysqlHost := os.Getenv("MYSQL_HOST")
 	mysqlPort := os.Getenv("MYSQL_PORT")
 	mysqlDatabase := os.Getenv("MYSQL_DATABASE")
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", mysqlUser, mysqlPassword, mysqlHost, mysqlPort, mysqlDatabase)
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", mysqlUser, mysqlPassword, mysqlHost, mysqlPort, mysqlDatabase)
 	db, err := gorm.Open("mysql", dsn)
 	if err != nil {
 		log.Fatalln(err)
@@ -49,6 +49,28 @@ func main() {
 	router.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("pong"))
+	})
+
+	router.HandleFunc("/api/v1/posts/{id:[0-9]+}", func(w http.ResponseWriter, r *http.Request) {
+		// GETリクエストのみ受け付ける
+		if r.Method != "GET" {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+
+		vars := mux.Vars(r)
+		id := vars["id"]
+		post := Post{}
+		if err := db.First(&post, "id=?", id).Error; gorm.IsRecordNotFoundError(err) {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+
+		// header → status code → response body の順番にしないと無効になる
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		res, _ := json.Marshal(post)
+		w.Write(res)
 	})
 
 	router.HandleFunc("/api/v1/posts", func(w http.ResponseWriter, r *http.Request) {
