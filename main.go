81d6d44d47b1bbcd957ff029c01ebfccfbb953b0
roaris <one0803kyopro@gmail.com>
2022-01-29 03:45:11 +0900

    認証のミドルウェアを追加

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/roaris/go_sns_api/handlers"
+	"github.com/roaris/go_sns_api/middlewares"
 )
 
 func main() {
@@ -16,9 +17,9 @@ func main() {
 
 	v1r := r.PathPrefix("/api/v1").Subrouter()
 	v1r.Methods(http.MethodGet).Path("/posts/{id:[0-9]+}").HandlerFunc(handlers.PostShow)
-	v1r.Methods(http.MethodPost).Path("/posts").HandlerFunc(handlers.PostCreate)
-	v1r.Methods(http.MethodPatch).Path("/posts/{id:[0-9]+}").HandlerFunc(handlers.PostUpdate)
-	v1r.Methods(http.MethodDelete).Path("/posts/{id:[0-9]+}").HandlerFunc(handlers.PostDelete)
+	v1r.Methods(http.MethodPost).Path("/posts").HandlerFunc(middlewares.AuthMiddleware(handlers.PostCreate))
+	v1r.Methods(http.MethodPatch).Path("/posts/{id:[0-9]+}").HandlerFunc(middlewares.AuthMiddleware(handlers.PostUpdate))
+	v1r.Methods(http.MethodDelete).Path("/posts/{id:[0-9]+}").HandlerFunc(middlewares.AuthMiddleware(handlers.PostDelete))
 	v1r.Methods(http.MethodPost).Path("/users").HandlerFunc(handlers.UserCreate)
 	v1r.Methods(http.MethodPost).Path("/auth").HandlerFunc(handlers.Authenticate)
 	http.ListenAndServe(":8080", r)
---
+++ middlewares/auth.go
@@ -0,0 +1,59 @@
+package middlewares
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+// ヘッダからトークンを取得する
+func getTokenFromHeader(r *http.Request) (string, error) {
+	header := r.Header.Get("Authorization")
+	if header == "" {
+		return "", errors.New("authorization header not found")
+	}
+	bearer := "Bearer"
+	l := len(bearer)
+	if len(header) > l && header[:l] == bearer {
+		return header[l+1:], nil
+	}
+	return "", errors.New("invalid format token")
+}
+
+// JWTトークンの検証を行う
+func parseToken(signedString string) (int, error) {
+	token, err := jwt.Parse(signedString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, errors.New("unexpected signing method")
+		}
+		return []byte(os.Getenv("SECRET")), nil
+	})
+	if err != nil {
+		return -1, err
+	}
+	claims := token.Claims.(jwt.MapClaims)
+	userID := claims["sub"].(float64)
+	return int(userID), err
+}
+
+// JWTトークンの検証を行うミドルウェア
+func AuthMiddleware(handler http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		token, err := getTokenFromHeader(r)
+		if err != nil {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		userID, err := parseToken(token)
+		if err != nil {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		fmt.Println(userID)
+		// todo: userIDをContextにセットする
+		handler(w, r)
+	}
+}
