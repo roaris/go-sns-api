493640f2f7be1ba84558cf57fcbe580d69d61cee
roaris <one0803kyopro@gmail.com>
2022-02-12 21:39:08 +0900

    postHandlerのShowのテスト追加

--- handlers/post_test.go
+++ handlers/post_test.go
@@ -3,8 +3,11 @@ package handlers
 import (
 	"fmt"
 	"net/http/httptest"
+	"strconv"
 	"strings"
 	"testing"
+
+	"github.com/gorilla/mux"
 
 	"github.com/roaris/go-sns-api/httputils"
 
@@ -70,5 +73,50 @@ func TestCreatePost(t *testing.T) {
 		// after
 		db.Model(&models.Post{}).Count(&count)
 		assert.Equal(t, 0, count)
+	})
+}
+
+func TestShowPost(t *testing.T) {
+	t.Run("ok", func(t *testing.T) {
+		db := models.CreateTestDB()
+		defer models.CleanUpTestDB(db)
+
+		user, _ := models.CreateUser(db, "taro", "taro@example.com", "password")
+		post, _ := models.CreatePost(db, user.ID, "I'm happy.")
+
+		postHandler := NewPostHandler(db)
+		r := httptest.NewRequest("GET", fmt.Sprintf("/api/v1/posts/%d", post.ID), nil)
+		vars := map[string]string{
+			"id": strconv.FormatInt(post.ID, 10),
+		}
+		r = mux.SetURLVars(r, vars)
+		w := httptest.NewRecorder()
+		status, payload, err := postHandler.Show(w, r)
+
+		assert.Equal(t, 200, status)
+		assert.Equal(t, post.Content, payload.(gen.PostAndUser).Post.Content)
+		assert.Equal(t, user.Name, payload.(gen.PostAndUser).User.Name)
+		assert.Equal(t, nil, err)
+	})
+
+	t.Run("not found", func(t *testing.T) {
+		db := models.CreateTestDB()
+		defer models.CleanUpTestDB(db)
+
+		user, _ := models.CreateUser(db, "taro", "taro@example.com", "password")
+		post, _ := models.CreatePost(db, user.ID, "I'm happy.")
+
+		postHandler := NewPostHandler(db)
+		r := httptest.NewRequest("GET", fmt.Sprintf("/api/v1/posts/%d", post.ID+1), nil)
+		vars := map[string]string{
+			"id": strconv.FormatInt(post.ID+1, 10),
+		}
+		r = mux.SetURLVars(r, vars)
+		w := httptest.NewRecorder()
+		status, payload, err := postHandler.Show(w, r)
+
+		assert.Equal(t, 404, status)
+		assert.Equal(t, nil, payload)
+		assert.NotEqual(t, nil, err)
 	})
 }
