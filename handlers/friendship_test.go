9a76ef9f302636f127e119fb58bcd085ff02619b
roaris <one0803kyopro@gmail.com>
2022-02-13 23:04:59 +0900

    friendshipHandlerのDestroyのテスト追加

--- handlers/friendship_test.go
+++ handlers/friendship_test.go
@@ -225,3 +225,69 @@ func TestShowFollowers(t *testing.T) {
 		assert.NotEqual(t, nil, err)
 	})
 }
+
+func TestDestroyFollowees(t *testing.T) {
+	t.Run("ok", func(t *testing.T) {
+		db := models.CreateTestDB()
+		defer models.CleanUpTestDB(db)
+
+		user1, _ := models.CreateUser(db, "alice", "alice@example.com", "password")
+		user2, _ := models.CreateUser(db, "bob", "bob@example.com", "password")
+		models.CreateFollowee(db, user1.ID, user2.ID)
+
+		// before
+		beforeFollowees, _ := models.GetFollowees(db, user1.ID)
+		assert.Equal(t, 1, len(beforeFollowees))
+
+		// フォロー解除する
+		friendshipHandler := NewFriendshipHandler(db)
+		r := httptest.NewRequest("DELETE", fmt.Sprintf("/api/v1/users/me/followees/%d", user2.ID), nil)
+		vars := map[string]string{
+			"id": strconv.FormatInt(user2.ID, 10),
+		}
+		r = mux.SetURLVars(r, vars)
+		ctx := httputils.SetUserIDToContext(r.Context(), user1.ID)
+		w := httptest.NewRecorder()
+		status, payload, err := friendshipHandler.Destroy(w, r.WithContext(ctx))
+
+		assert.Equal(t, 204, status)
+		assert.Equal(t, nil, payload)
+		assert.Equal(t, nil, err)
+
+		// after
+		afterFollowees, _ := models.GetFollowees(db, user1.ID)
+		assert.Equal(t, 0, len(afterFollowees))
+	})
+
+	t.Run("not found", func(t *testing.T) {
+		db := models.CreateTestDB()
+		defer models.CleanUpTestDB(db)
+
+		user1, _ := models.CreateUser(db, "alice", "alice@example.com", "password")
+		user2, _ := models.CreateUser(db, "bob", "bob@example.com", "password")
+		models.CreateFollowee(db, user1.ID, user2.ID)
+
+		// before
+		beforeFollowees, _ := models.GetFollowees(db, user1.ID)
+		assert.Equal(t, 1, len(beforeFollowees))
+
+		// フォロー解除する(存在しないユーザーを指定しているので失敗)
+		friendshipHandler := NewFriendshipHandler(db)
+		r := httptest.NewRequest("DELETE", fmt.Sprintf("/api/v1/users/me/followees/%d", user2.ID+1), nil)
+		vars := map[string]string{
+			"id": strconv.FormatInt(user2.ID+1, 10),
+		}
+		r = mux.SetURLVars(r, vars)
+		ctx := httputils.SetUserIDToContext(r.Context(), user1.ID)
+		w := httptest.NewRecorder()
+		status, payload, err := friendshipHandler.Destroy(w, r.WithContext(ctx))
+
+		assert.Equal(t, 404, status)
+		assert.Equal(t, nil, payload)
+		assert.NotEqual(t, nil, err)
+
+		// after
+		afterFollowees, _ := models.GetFollowees(db, user1.ID)
+		assert.Equal(t, 1, len(afterFollowees))
+	})
+}
